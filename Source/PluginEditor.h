4618a9151da883f40a93d85c6d2b9a4c452f16b3
Eliza Sohn <eliza.sohn@gmail.com>
2022-06-05 04:43:35 -0700

    create slider variables

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -34,8 +34,23 @@ private:
     // OUR VARIABLES ===============================================================
     juce::Label mMidiDisplay;
     juce::Label mEditorLogger;
+    // gain
+    juce::Slider gainSlider;
+    // asdr
+    juce::Slider attackSlider;
+    juce::Slider decaySlider;
+    juce::Slider sustainSlider;
+    juce::Slider releaseSlider;
+    // osc
+    juce::ComboBox oscSelector;
 
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> gainSliderAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> attackAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> decayAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> sustainAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> releaseAttachment;
 
-
+    std::unique_ptr<juce::AudioProcessorValueTreeState::ComboBoxAttachment> oscSelAttachment;
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FreshSynthAudioProcessorEditor)
 };
