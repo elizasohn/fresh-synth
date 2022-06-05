1dba90c69532b4a90538f95bdaa8087e0c8cd954
Eliza Sohn <eliza.sohn@gmail.com>
2022-06-05 04:42:51 -0700

    create params

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -22,7 +22,7 @@ FreshSynthAudioProcessor::FreshSynthAudioProcessor()
                       #endif
                        .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
                      #endif
-                       )
+                       ), apvts(*this, nullptr, "parameters", createParameters())
 #endif
 {
     // Get Default MIDI name and display. 
@@ -250,18 +250,20 @@ juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
     return new FreshSynthAudioProcessor();
 }
 
-//// Synth Parameter Value Tree
-//juce::AudioProcessorValueTreeState::ParameterLayout FreshSynthAudioProcessor::createParams()
-//{
-//    std::vector<std::unique_ptr<juce::RangedAudioParameter>> params;
-//    
-//    // Osc Select
-//    params.push_back(std::make_unique<juce::AudioParameterChoice> ("OSC", "Oscillator", juce::StringArray {"Sine", "Saw", "Square" }, 0));
-//    
-//    // ADSR
-//    params.push_back (std::make_unique<juce::AudioParameterFloat>("ATTACK", "Attack", juce::NormalisableRange<float> { 0.1f, 1.0f, }, 0.1f));
-//    params.push_back (std::make_unique<juce::AudioParameterFloat>("DECAY", "Decay", juce::NormalisableRange<float> { 0.1f, 1.0f, }, 0.1f));
-//    params.push_back (std::make_unique<juce::AudioParameterFloat>("SUSTAIN", "Sustain", juce::NormalisableRange<float> { 0.1f, 1.0f, }, 0.1f));
-//    params.push_back (std::make_unique<juce::AudioParameterFloat>("RELEASE", "Release", juce::NormalisableRange<float> { 0.1f, 1.0f, }, 0.1f));
-//    return { params.begin(), params.end() };
-//}
+// Synth Parameter Value Tree
+juce::AudioProcessorValueTreeState::ParameterLayout FreshSynthAudioProcessor::createParameters()
+{
+    std::vector<std::unique_ptr<juce::RangedAudioParameter>> params;
+    
+    // Gain
+    params.push_back(std::make_unique<juce::AudioParameterFloat>("GAIN", "Gain", 0.0f, 1.0f, 0.5f));
+    // Osc Select
+    params.push_back(std::make_unique<juce::AudioParameterChoice> ("OSC", "Oscillator", juce::StringArray {"Sine", "Saw", "Square" }, 0));
+    
+    // ADSR
+    params.push_back (std::make_unique<juce::AudioParameterFloat>("ATTACK", "Attack", juce::NormalisableRange<float> { 0.1f, 1.0f, }, 0.1f));
+    params.push_back (std::make_unique<juce::AudioParameterFloat>("DECAY", "Decay", juce::NormalisableRange<float> { 0.1f, 1.0f, }, 0.1f));
+    params.push_back (std::make_unique<juce::AudioParameterFloat>("SUSTAIN", "Sustain", juce::NormalisableRange<float> { 0.1f, 2.0f, }, 0.1f));
+    params.push_back (std::make_unique<juce::AudioParameterFloat>("RELEASE", "Release", juce::NormalisableRange<float> { 0.1f, 3.0f, }, 0.3f));
+    return { params.begin(), params.end() };
+}
