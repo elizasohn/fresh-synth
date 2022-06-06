50c5b26c5acb378353385dedfb4989e5dc42e530
Eliza Sohn <eliza.sohn@gmail.com>
2022-06-06 16:23:25 -0700

    comment out set level

--- Source/SynthVoice.h
+++ Source/SynthVoice.h
@@ -9,6 +9,7 @@ public:
 	bool canPlaySound(juce::SynthesiserSound* sound) override;
 	void startNote(int midiNoteNumber, float velocity, juce::SynthesiserSound* sound, int currentPitchWheelPosition) override;
 	void stopNote(float velocity, bool allowTailOff) override;
+//    void setLevel(const float gain);
 	void controllerMoved(int controllerNumber, int newControllerValue) override;
 	void pitchWheelMoved(int newPitchWheelValue) override;
 	void prepareToPlay(double sampleRate, int samplesPerBlock, int outputChannels);
